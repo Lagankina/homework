8141b74f3d52f4cb0f69efc538bff472f683c886
Lagankina <reirianet@mail.ru>
2023-02-25 15:50:18 +0300

    1-ое домашнее задание от 25 февраля 2023Э

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу , которая на вход принимает число, и выдает, является  ли число четным
+Console.WriteLine("Введите число number: ");
+int number = int.Parse (Console.ReadLine());
+
+ if ( number%2==0 ) {
+    Console.WriteLine("Число " + number + " четное" );
+ }  
+ else {
+    Console.WriteLine("Число " + number + " нечетное");
+ }
