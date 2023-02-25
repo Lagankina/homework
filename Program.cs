13ac6bb9818bd76507c5c09883445698a1316df2
Lagankina <reirianet@mail.ru>
2023-02-25 15:45:14 +0300

    2-ое домашнее задание от 24 февраля 2023

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу,которая принимает на вход три числа и выдает максимальное из этих чисел
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (max < b) {
+    max = b;
+}
+if (max < c) {
+    max = c;
+}
+
+Console.WriteLine("Max = " + max);
