e2ffc2c51ce71748dd14934aa5ff55c4c99c37e6
Lagankina <reirianet@mail.ru>
2023-02-25 15:52:17 +0300

    2-ое домашнее задание от 25 февраля 2023

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число (N),  а на выходе показывает все четные числа от 1 до N
+Console.WriteLine("Введите положительное число: ");
+int n = int.Parse (Console.ReadLine());
+
+for (int number = 2; number < n; number++) {
+    if (number%2==0)
+        Console.WriteLine (number);
+}
+
