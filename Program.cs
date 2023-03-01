9a30154ffdbc72be40cd1aec16505a29ae006d10
Lagankina <reirianet@mail.ru>
2023-03-01 14:25:52 +0300

    Домашнее задание №1 к семинару №2

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите трехзначное число number: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = (number%100/10);
+
+Console.WriteLine(result);
