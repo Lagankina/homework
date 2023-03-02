12cb2412615ec0f2a950aa5deb995d69de878702
Lagankina <reirianet@mail.ru>
2023-03-02 16:48:00 +0300

    Домашнее задание 2 (13)

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = -1;
+
+if(number>99) {
+    while (number > 999) {
+      number = number / 10;
+    }
+    result = number % 10; 
+} else {
+     Console.WriteLine("Третьего числа нет "); 
+}
+Console.WriteLine(result) ;
