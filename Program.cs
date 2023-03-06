32ec7a199f820652a30a06a056d249ee310badae
Lagankina <reirianet@mail.ru>
2023-03-06 16:02:54 +0300

    Задание 3 семинар

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// ... которая прнимает на вход число (N), и выдает таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+
+for(int i = 1; i <= N; i++ ) {
+   
+  Console.WriteLine((Math.Pow(i,3)));
+ }
+
+
