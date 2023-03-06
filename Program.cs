48543b8b9714435f15422d7ac87faf4759686062
Lagankina <reirianet@mail.ru>
2023-03-06 14:37:42 +0300

    Задача 2 к семинару

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// ... котора принимает на вход координаты двух точек и находит расстояние между ними в 3д пространстве
+Console.WriteLine("Введите координаты точки А: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки А: ");
+int y = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки А: ");
+int z = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки В: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки В: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки В: ");
+int z1 = int.Parse(Console.ReadLine());
+
+int A = x - x1;
+int B = y - y1;
+int C = z - z1;
+
+double result = Math.Sqrt(Math.Pow(A,2) + Math.Pow(B,2) + Math.Pow(C,2));
+
+Console.WriteLine(result);
