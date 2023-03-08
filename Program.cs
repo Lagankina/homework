77549ab6691c833d145af50e6ef84dac6478496b
Lagankina <reirianet@mail.ru>
2023-03-08 13:52:44 +0300

    Задание 2 семинар 06 марта

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+// int count = getNumCount(number);
+
+// int[] array = new int[count];
+int tmp = number;
+int sum = 0;
+// for (int index = 0; index < count; index++)
+// {
+//     sum += tmp % 10;
+//     tmp = tmp / Math.Pow(10, index);
+// }
+
+int index = 0;
+while(tmp > 0)
+{
+    sum += tmp % 10;
+    tmp = (int) (number / Math.Pow(10, ++index));
+}
+Console.WriteLine(sum);
+
+// static int getNumCount(int a) {
+//     int count = 0;
+//     while((a / Math.Pow(10, count)) > 0)
+//     {
+//         count++;
+//     }
+
+//     return count;
+// }
