d7a7641834c9e04edb8a78889a3c0bfc20734589
Lagankina <reirianet@mail.ru>
2023-03-08 14:51:29 +0300

    Задание 3 семинар 6 марта

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// ... которая задает массив из 8 элементов и выводит их а экран
+
+//int[] array = new Random[8];
+//Console.WriteLine(array);
+
+int[] array = new int[8];
+
+for (int index = 0; index < 8; index++) {
+    array[index] = new Random().Next();
+    Console.WriteLine(array[index]);
+}
+
