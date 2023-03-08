40febb962a1dc6242100d99aa0408efa2fdfc635
Lagankina <reirianet@mail.ru>
2023-03-08 22:47:16 +0300

    задача 1 саминара от 06_03_2023

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// 
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int result = 1;
+
+for(int i = 0; i < number2; i++)
+{
+    result = result * number1; 
+}
+Console.WriteLine(result);
+
