cd7e14f600ce73d553f0662ace22ec6fd640a4f3
Lagankina <reirianet@mail.ru>
2023-03-15 17:06:50 +0300

    3 задача от 9 марта 2023

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив вещественных чисел. 
+//нАйдите разницу между максимальным и минимальным элементом массива.
+
+int[] myArray = generateRandomIntArray(5, -100, 100);
+Console.WriteLine("Сгенерированный массив: ");
+printArray(myArray);
+Console.WriteLine();
+
+int minValue = myArray[0];
+int maxValue = myArray[0];
+
+for (int index = 0; index < myArray.Length; index++) 
+{
+    if(myArray[index] < minValue)
+    {
+        minValue = myArray[index];
+    } else if (myArray[index] > maxValue)
+    {
+        maxValue = myArray[index];
+    }
+}
+
+Console.WriteLine("Разница между минимальным(" + minValue + ") и максимальным(" + maxValue + ") значениями: " + (maxValue - minValue));
+
+int[] generateRandomIntArray(int capacity, int minValue, int maxValue)
+{
+    int[] myArray = new int[capacity];
+    for(int i = 0; i < myArray.Length; i++) {
+         myArray[i] = new Random().Next(minValue, maxValue);
+    }
+    return myArray;
+}
+
+
+void printArray(int[] array) {
+    for(int i = 0; i < array.Length; i++) {
+        Console.Write(array[i] + " ");
+    }
+} 
