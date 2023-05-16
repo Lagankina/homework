2523e8a3155a1d60254c850612deb25c883a7a34
Lagankina Ekaterina <reirianet@mail.ru>
2023-05-16 22:01:02 +0300

    Двумерные массивы рандом

--- Program.cs
+++ Program.cs
@@ -398,4 +398,17 @@
 // Console.WriteLine("точка пересечения: " + (x, y));
 
 
+//семинар от 16 мая//
 
+// int[,] arr = new int[2,3];
+
+// for(int i = 0; i < arr.GetLength(0); i++)
+//     {
+//     for(int j = 0; j < arr.GetLength(1); j++)
+//     {
+//         arr[i,j] = new Random().Next(0,10);
+//         Console.Write(arr[i,j]);
+//     }
+//     Console.WriteLine();
+
+// }
